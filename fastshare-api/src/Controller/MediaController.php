46e99dfc3217d38cec6ad2da82ba7735308f502f
Justin Passchier <juspasschier@gmail.com>
2022-12-23 15:58:21 +0100

    Added first setup for FastShare-api

---
+++ fastshare-api/src/Controller/MediaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MediaController extends AbstractController
+{
+    /**
+     * @Route("/media", name="app_media")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MediaController.php',
+        ]);
+    }
+}
