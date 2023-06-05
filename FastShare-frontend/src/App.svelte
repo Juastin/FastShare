5d1dbb5f54442ff5dd93296aa49f215633d716f3
Justin Paschier <juspasschier@gmail.com>
2023-06-05 23:00:50 +0200

    Added form with multipart/formdata for backend

--- FastShare-frontend/src/App.svelte
+++ FastShare-frontend/src/App.svelte
@@ -1,47 +1,26 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <p>Hoi, dit is FastShare</p>
+  <form action="file" enctype="multipart/form-data" method="post" autocomplete="off">
+    <div class="mb-3">
+        <div class="bg-light p-5 rounded-lg m-3">
+            <label for="file" class="form-label">File Input</label>
+            <input class="form-control" type="file" id="file" name="file">
+        </div>
+    </div>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <div class="bg-light rounded-lg m-3">
+        <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit"><i class="bx bxs-cloud-upload"
+                    style="font-size:1.4em;"></i> Upload!</button>
+        </div>
+    </div>
+</form>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
